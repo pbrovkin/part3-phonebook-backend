f7dcb06d287be2afc3ea4ee176e43122880c969e
pbrovkin <pbrovkin@gmail.com>
2020-05-25 14:32:33 +0300

    phonebook-backend, step1

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let contacts = [
+    {
+        name: 'Arto Hellas',
+        number: '040-123456'
+    },
+    {
+        name: 'Ada Lovelace',
+        number: '39-44-5323523'
+    },
+    {
+        name: 'Dan Abramov',
+        number: '12-43-234345'
+    },
+    {
+        name: 'Mary Poppendieck',
+        number: '39-23-6423122'
+    }
+]
+
+
+app.get('/api/contacts', (req, res) => {
+    res.json(contacts)
+})
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
