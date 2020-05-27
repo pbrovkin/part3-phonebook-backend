417b65c0c519617002fb53c41e9bd988dce6ed5a
pbrovkin <pbrovkin@gmail.com>
2020-05-27 11:46:34 +0300

    heroku commit

---
+++ build/precache-manifest.3e3acf29d3765239b6e6a03ebed987b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "205937cf3c786e34e9942b8f30e0199e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da9c83f8379307f89c63",
+    "url": "/static/js/2.6de0d325.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6de0d325.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e14e51cba707d8720b3d",
+    "url": "/static/js/main.87deab90.chunk.js"
+  },
+  {
+    "revision": "37e42bc47805ba865915",
+    "url": "/static/js/runtime-main.fac6ad05.js"
+  }
+]);
